973f910a45b52aeb0a3ee7e85a0801cb06e43ec1
IrinaMuravyeva <miomir@yandex.ru>
2026-06-08 14:17:35 +0800

    Initial Commit

---
+++ Travel_schedule/Travel_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_scheduleApp.swift
+//  Travel_schedule
+//
+//  Created by Irina Muravyeva on 08.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Travel_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
