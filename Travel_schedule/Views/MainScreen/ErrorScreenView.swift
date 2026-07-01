c7de957918263cd92c32d3cb4cabaf41c19ab5d0
IrinaMuravyeva <miomir@yandex.ru>
2026-07-01 23:50:51 +0800

    add errors views for main screen

---
+++ Travel_schedule/Views/MainScreen/ErrorScreenView.swift
@@ -0,0 +1,29 @@
+//
+//  ErrorScreenView.swift
+//  Travel_schedule
+//
+//  Created by Irina Muravyeva on 29.06.2026.
+//
+
+import SwiftUI
+
+enum NetworkError {
+    case serverNotAllow
+    case connectionError
+}
+
+struct ErrorScreenView: View {
+    @State private var error: NetworkError?
+    @State private var isPresented: Bool = false
+    
+    var body: some View {
+        switch error {
+        case .serverNotAllow:
+            ErrorView(image: .serverError, text: "Server error")
+        case .connectionError:
+            ErrorView(image: .noInternet, text: "No internet")
+        default:
+            Text("Unknown error")
+        }
+    }
+}
