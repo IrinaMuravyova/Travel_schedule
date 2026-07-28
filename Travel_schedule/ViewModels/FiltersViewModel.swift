0938895a7a654af7d4c101c54739fa949d2e1c9f
IrinaMuravyeva <miomir@yandex.ru>
2026-07-28 12:57:37 +0800

    refactor: add FiltersViewModel

--- Travel_schedule/ViewModels/FiltersViewModel.swift
+++ Travel_schedule/ViewModels/FiltersViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Combine
+
+@MainActor
+final class FiltersViewModel: ObservableObject {
+    @Published var selectedTimeSlots: Set<TimeSlot>
+    @Published var transferFilter: TransferFilter?
+    
+    init(
+        selectedTimeSlots: Set<TimeSlot> = [],
+        transferFilter: TransferFilter? = nil
+    ) {
+        self.selectedTimeSlots = selectedTimeSlots
+        self.transferFilter = transferFilter
+    }
+    
+    var isAnyFilterSelected: Bool {
+        !selectedTimeSlots.isEmpty || transferFilter != nil
+    }
+    
+    func toggleTimeSlot(_ slot: TimeSlot) {
+        if selectedTimeSlots.contains(slot) {
+            selectedTimeSlots.remove(slot)
+        } else {
+            selectedTimeSlots.insert(slot)
+        }
+    }
+    
+    func selectTransferFilter(_ filter: TransferFilter) {
+        transferFilter = filter
+    }
+    
+    func clearFilters() {
+        selectedTimeSlots.removeAll()
+        transferFilter = nil
+    }
+}
